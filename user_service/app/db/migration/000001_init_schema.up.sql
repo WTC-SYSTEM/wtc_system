c9696597cd4a937cbfaa3bcadc7be9b8658d884f
mlazebny <miskadl09@gmail.com>
2022-04-07 20:40:46 +0300

    add migrations

--- user_service/app/db/migration/000001_init_schema.up.sql
+++ user_service/app/db/migration/000001_init_schema.up.sql
@@ -1,10 +1,7 @@
-create table public.user
+create table "user"
 (
     id       uuid primary key default gen_random_uuid(),
     username varchar(100) not null,
     email    varchar(50)  unique not null,
     password varchar(100) not null
 );
-
-alter table public.user
-add constraint UQ_user_username UNIQUE(username)
