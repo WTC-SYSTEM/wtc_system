da2f9feed53b3e944670684535a822719ab8d073
mlazebny <miskadl09@gmail.com>
2022-04-18 22:59:09 +0300

    [#3] final test

--- user_service/app/cmd/main/app.go
+++ user_service/app/cmd/main/app.go
@@ -21,7 +21,6 @@ import (
 func main() {
 	logging.Init()
 	logger := logging.GetLogger()
-
 	logger.Println("logger initialized")
 	logger.Println("config initializing")
 	cfg := config.GetConfig()
