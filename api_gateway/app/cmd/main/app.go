dcecfad5f5e34c5496b2a2c70d06a04b260b6164
mlazebny <miskadl09@gmail.com>
2022-04-19 23:34:47 +0300

    [hotfix]

--- api_gateway/app/cmd/main/app.go
+++ api_gateway/app/cmd/main/app.go
@@ -11,11 +11,8 @@ import (
 	"github.com/hawkkiller/wtc_system/api_gateway/pkg/jwt"
 	"github.com/hawkkiller/wtc_system/api_gateway/pkg/logging"
 	"github.com/hawkkiller/wtc_system/api_gateway/pkg/shutdown"
-	"net"
 	"net/http"
 	"os"
-	"path"
-	"path/filepath"
 	"syscall"
 	"time"
 )
@@ -51,46 +48,30 @@ func main() {
 	start(router, logger, cfg)
 }
 
-func start(router *mux.Router, logger logging.Logger, cfg *config.Config) {
+func start(router http.Handler, logger logging.Logger, cfg *config.Config) {
 	var server *http.Server
-	var listener net.Listener
-
-	if cfg.Listen.Type == "sock" {
-		appDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-		if err != nil {
-			logger.Fatal(err)
-		}
-		socketPath := path.Join(appDir, "app.sock")
-		logger.Infof("socket path: %s", socketPath)
-
-		logger.Info("create and listen unix socket")
-		listener, err = net.Listen("unix", socketPath)
-		if err != nil {
-			logger.Fatal(err)
-		}
-	} else {
-		logger.Infof("bind application to host: %s and port: %s", cfg.Listen.BindIP, cfg.Listen.Port)
-
-		var err error
-
-		listener, err = net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Listen.BindIP, cfg.Listen.Port))
-		if err != nil {
-			logger.Fatal(err)
-		}
-	}
 
 	server = &http.Server{
+		Addr:         fmt.Sprintf(":%s", cfg.Listen.Port),
 		Handler:      router,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
-	go shutdown.Graceful([]os.Signal{syscall.SIGABRT, syscall.SIGQUIT, syscall.SIGHUP, os.Interrupt, syscall.SIGTERM},
-		server)
+	go shutdown.Graceful(
+		[]os.Signal{
+			syscall.SIGABRT,
+			syscall.SIGQUIT,
+			syscall.SIGHUP,
+			syscall.SIGTERM,
+			os.Interrupt,
+		},
+		server,
+	)
 
 	logger.Println("application initialized and started")
 
-	if err := server.Serve(listener); err != nil {
+	if err := server.ListenAndServe(); err != nil {
 		switch {
 		case errors.Is(err, http.ErrServerClosed):
 			logger.Warn("server shutdown")
@@ -98,4 +79,5 @@ func start(router *mux.Router, logger logging.Logger, cfg *config.Config) {
 			logger.Fatal(err)
 		}
 	}
+
 }
