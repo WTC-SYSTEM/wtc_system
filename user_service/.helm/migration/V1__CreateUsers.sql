39f79d62e87d3805e0a916e1958fb48c7b80234b
mlazebny <miskadl09@gmail.com>
2022-04-30 17:12:37 +0300

    change migration

--- user_service/.helm/migration/V1__CreateUsers.sql
+++ user_service/.helm/migration/V1__CreateUsers.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "users"
 (
-    id       BIGSERIAL PRIMARY KEY,
+    id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR(100)       NOT NULL,
     email    VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(100)       NOT NULL
