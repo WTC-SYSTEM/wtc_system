75050ede7a129a519a672bc1df45163413c89ff1
mlazebny <miskadl09@gmail.com>
2022-04-30 12:48:48 +0300

    bigserial

--- user_service/.helm/migration/V1__CreateUsers.sql
+++ user_service/.helm/migration/V1__CreateUsers.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "users"
 (
-    id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id       BIGSERIAL PRIMARY KEY,
     username VARCHAR(100)       NOT NULL,
     email    VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(100)       NOT NULL
