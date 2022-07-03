e8aa6ae2311599ecdfec66ce3504b13b853a2d0a
mlazebny <miskadl09@gmail.com>
2022-07-03 20:16:59 +0300

    [#47]: added swagger docs

--- api_gateway/app/internal/public/swagger-initializer.js
+++ api_gateway/app/internal/public/swagger-initializer.js
@@ -1,20 +1,33 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
+window.onload = function () {
+    //<editor-fold desc="Changeable Configuration Block">
 
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    url: "http://localhost:8080/public/swagger.json",
-    dom_id: '#swagger-ui',
-    deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
-  });
+    // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+    window.ui = SwaggerUIBundle({
+        url: "http://www.michaeldeveloper.com/public/swagger.json",
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl,
+            DisableTryItOutPlugin
+        ],
+        layout: "StandaloneLayout"
+    });
 
-  //</editor-fold>
+    //</editor-fold>
 };
+
+const DisableTryItOutPlugin = function () {
+    return {
+        statePlugins: {
+            spec: {
+                wrapSelectors: {
+                    allowTryItOutFor: () => () => false
+                }
+            }
+        }
+    }
+}
